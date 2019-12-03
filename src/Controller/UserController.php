7c77ef46a821bb593358241daf2fa9811109e588
eleve <eleve@z210>
2019-12-03 13:10:22 +0100

    Ajout liste modif

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -47,12 +47,36 @@ class UserController extends AbstractController
      * @Route("/user_liste", name="user_liste")   
      */    public function liste(Request $request) 
              {     
-         $repository = $this->getDoctrine()->getManager()->getRepository(Utilisateur::class);  
+         $repository = $this->getDoctrine()->getManager()->getRepository(User::class);  
          $listeUser = $repository->findAll();    
          return $this->render('user/liste.html.twig', [  
              'listeUser' => $listeUser, 
              ]);
      }
      
+     /**
+     * @Route("/user_modifier/{id}", name="user_modifier")*/  
+     public function modifier(Request $request) 
+             {     
+         $repository = $this->getDoctrine()->getManager()->getRepository(User::class); 
+         $user = $repository->find($request->get('id'));
+         $form = $this->createFormBuilder($user)
+                 ->add('nom', TextType::class)
+                 ->add('prenom', TextType::class)
+                 ->add('save', SubmitType::class, array('label' => 'Modifier'))  
+                 ->getForm();
+         
+          if ($request->isMethod('POST')){   
+              $form -> handleRequest ($request);
+               if($form->isValid()){        
+                   $em = $this->getDoctrine()->getManager();
+                   $em->persist($user);         
+                   $em->flush();
+               }
+          }
+            return $this->render('user/modifier.html.twig', ['form'=>$form->createView()]);   
+            }
+     }
      
-}
+     
+
